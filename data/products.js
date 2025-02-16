72767601e50dcdc49d3dc6213148f4e86c359736
szpadel <szpadel30@gmail.com>
2025-02-16 16:18:36 +0100

    render products using js

--- data/products.js
+++ data/products.js
@@ -9,13 +9,13 @@ export function getProduct (productId) {
     });
     return matchingProduct
 }
-
+/*
 const uid = function(){
   return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
 
 console.log(uid())
-
+*/
 
 
 class Product {
@@ -45,42 +45,27 @@ class Jewlery extends Product {
   
  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  export let products = [{
-  id: "as9d8f7-a9s8dyfh-klj23h41-f9as8d7f-4klj12bh34",
-  image: '../images/productimages/naszyjnik1.jpg',
-  name: "Gold necklace with pearl and diamonds",
-  priceCents: 699900,
-  karats: '24',
+    id: "as9d8f7-a9s8dyfh-klj23h41-f9as8d7f-4klj12bh34",
+    image: '../images/productimages/naszyjnik1.jpg',
+    name: "Gold necklace with pearl and diamonds",
+    priceCents: 699900,
+    karats: '24kt',
   },{
     id: "kj1234hn-bl15io9-a098sdyrh-23i4hgvbo-8ffsekmhw",
-    image: '../images/productimages/naszyjnik1.jpg',
-    name: "Gold necklace with pearl and diamonds",
-    priceCents: 699900,
-    karats: '24',
+    image: '../images/productimages/white-purse.png',
+    name: "GG Marmont small shoulder bag",
+    priceCents: 220000,
   },{
-    id: "as9d8f7-a9s8dyfh-klj23h41-f9as8d7f-4klj12bh34",
-    image: '../images/productimages/naszyjnik1.jpg',
-    name: "Gold necklace with pearl and diamonds",
-    priceCents: 699900,
-    karats: '24',
+    id: "kj1234hn-bl15io9-a098sdyrh-f9as8d7f-4klj12bh34",
+    image: '../images/productimages/torebka-louis-vuitton-saumur-bb.jpg',
+    name: "Louis Vuitton Samur bag",
+    priceCents: 299000,
+   
   },{
-    id: "as9d8f7-a9s8dyfh-klj23h41-f9as8d7f-4klj12bh34",
-    image: '../images/productimages/naszyjnik1.jpg',
-    name: "Gold necklace with pearl and diamonds",
-    priceCents: 699900,
-    karats: '24',
+    id: "a098sdyrh-f9as8d7f-klj23h41-f9as8d7f-4klj12bh34",
+    image: '../images/productimages/ZANK563.jpg',
+    name: "Mohan Jewellery Diamond Necklace",
+    priceCents: 80000,
+    karats: '18kt',
   }]
--- scripts/shop.js
+++ scripts/shop.js
@@ -1,11 +1,45 @@
 
 import { renderNavbarshop } from "./navbarShopRender.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 renderNavbarshop()
-
-
 
 document.querySelector('.js-toggle-sidebar').addEventListener('click', () => {
 
     document.querySelector('.js-filter-list').classList.toggle('open')
     document.querySelector('.js-hide').classList.toggle('open')
 })
+
+
+    let productsHTML = ``
+
+    products.forEach((product) => { 
+        productsHTML+=
+                    `
+        <div class="product-container">
+                        <div class="product-image-container">
+                            <img class="product-image" src="${product.image}">
+
+                        </div>
+                        <div class="product-name-container">${product.name}</div>
+                        <div class="product-price-container">$${formatCurrency(product.priceCents)}</div>
+                        <div class="karat-count-container">${product.karats}</div>
+                        <div class="add-to-cart-button-container">
+                            <button class="add-to-cart-button">add to cart</button>
+                            
+                        </div>            
+                        <div class="product-cart-quantity-background">
+                            <p class="product-cart-quantity">&#10004;</p>
+                        </div>
+                        
+
+                    </div>
+        `;
+        
+    })
+
+
+
+
+console.log(productsHTML)
+document.querySelector('.js-products-grid').innerHTML = productsHTML
